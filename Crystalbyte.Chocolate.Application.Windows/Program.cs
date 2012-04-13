3a63f0cd45a78e90db0dc180a58e782f5cf2b26f
Alexander Wieser <alexander.wieser@crystalbyte.de>
2012-04-13 22:01:09 +0200

    cleaned main program

---
+++ Crystalbyte.Chocolate.Application.Windows/Program.cs
@@ -0,0 +1,41 @@
+﻿#region Namespace Directives
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+using Crystalbyte.Chocolate.Scripting;
+using Crystalbyte.Chocolate.UI;
+
+#endregion
+
+namespace Crystalbyte.Chocolate {
+    internal static class Program {
+
+        /// <summary>
+        ///   The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main() {
+#if (DEBUG)
+            // use this only for debugging purposes, sp mode is not yet as stable as it should be
+            Framework.Settings.IsSingleProcess = true;
+#endif
+            var module = Assembly.GetExecutingAssembly().ManifestModule;
+            var success = Framework.Initialize(module, new AppDelegate());
+            if (!success) {
+                Debug.WriteLine("initialization failed");
+                return;
+            }
+
+            if (!Framework.IsRootProcess) {
+                return;
+            }
+
+            var index = new Uri("http://www.battleshipmovie.com/");
+            var process = new RenderProcess(new Window {StartupUri = index}, new BrowserDelegate());
+            Framework.Run(process);
+            Framework.Shutdown();
+        }
+    }
+}
