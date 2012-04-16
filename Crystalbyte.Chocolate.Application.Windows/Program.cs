8752373cb7e5b284db5346863e073c01dd59076d
Alexander Wieser <alexander.wieser@crystalbyte.de>
2012-04-16 21:58:01 +0200

    cleaned startup application

--- Crystalbyte.Chocolate.Application.Windows/Program.cs
+++ Crystalbyte.Chocolate.Application.Windows/Program.cs
@@ -20,13 +20,8 @@ namespace Crystalbyte.Chocolate {
         /// </summary>
         [STAThread]
         private static void Main() {
-            Framework.Settings.IsSingleProcess = true;
-            var a = new AppDelegate();
-            a.BrowserCreated += OnBrowserCreated;
-            a.IpcMessageReceived += OnIpcMessageReceived;
-            a.ProcessStarted += OnProcessStarted;
             var module = Assembly.GetExecutingAssembly().ManifestModule;
-            var success = Framework.Initialize(module, a);
+            var success = Framework.Initialize(module, new AppDelegate());
             if (!success) {
                 Debug.WriteLine("initialization failed");
                 return;
@@ -36,41 +31,10 @@ namespace Crystalbyte.Chocolate {
                 return;
             }
 
-            var b = new MyBrowserDelegate();
-            b.PageLoaded += OnPageLoaded;
-            var index = new Uri("http://www.graphicscardbenchmarks.com/page/webgl");
-            var renderer = new HtmlRenderer(new Window {StartupUri = index}, b);
+            var index = new Uri("http://www.battleshipmovie.com/#/home");
+            var renderer = new HtmlRenderer(new Window {StartupUri = index}, new BrowserDelegate());
             Framework.Run(renderer);
             Framework.Shutdown();
-        }
-
-        private static void OnPageLoaded(object sender, PageLoadedEventArgs e) {
-            Debug.WriteLine(e.Frame.Url);
-            const string text = "This message was sent across process boundaries.";
-            var bytes = Encoding.UTF8.GetBytes(text);
-            var ms = new MemoryStream(bytes);
-            e.Browser.SendIpcMessage(ProcessType.Renderer, new IpcMessage("Hello") {
-                Payload = ms
-            });
-        }
-
-        private static void OnProcessStarted(object sender, ProcessStartedEventArgs e) {
-            
-        }
-
-        private static void OnIpcMessageReceived(object sender, IpcMessageReceivedEventArgs e) {
-            using (var sr = new StreamReader(e.Message.Payload)) {
-                var text = sr.ReadToEnd();
-                
-            }
-        }
-
-        private static void OnBrowserCreated(object sender, BrowserEventArgs e) {
-            var names = e.Browser.FrameNames;
-            var keys = e.Browser.MainFrame.Context.Document.Keys;
-            foreach (var name in names) {
-                Debug.WriteLine(name);
-            }
         }
     }
 }
